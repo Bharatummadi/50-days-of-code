a4a76769239ff56224553b7fe70a44fe5860ec73
Bharatummadi <bharatummadi23@gmail.com>
2020-05-25 19:09:16 +0530

    Update The Captain's Room.py

--- Day9/The Captain's Room.py
+++ Day9/The Captain's Room.py
@@ -1,7 +1,3 @@
-# Problem: https://www.hackerrank.com/challenges/py-the-captains-room/problem
-# Score: 10
-
-
 k = int(input())
 room_number_list = list(map(int, input().split()))
 captain_room_number = (sum(set(room_number_list)) * k - sum(room_number_list)) // (k - 1)
