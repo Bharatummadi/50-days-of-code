8f956f65d70516727cba75b4d6e218c206f45e0d
Bharatummadi <bharatummadi23@gmail.com>
2020-05-25 19:06:01 +0530

    Update Set discard() remove() pop().py

--- Day8/Set discard() remove() pop().py
+++ Day8/Set discard() remove() pop().py
@@ -1,7 +1,3 @@
-# Problem: https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem
-# Score: 10
-
-
 n = int(input())
 s = set(map(int, input().split()))
 for i in range(int(input())):
