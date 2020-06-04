22f2b81c40ae3b84112fe9e94ffbbb45f2c2e15e
Bharatummadi <bharatummadi23@gmail.com>
2020-06-04 18:47:26 +0530

    Added DeleteNodeinLL.py

---
+++ Day018/DeleteNodeinLL.py
@@ -0,0 +1,8 @@
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node.next is not None:
+            node.val, node.next = node.next.val, node.next.next
